623632c36c5e636108262ee642dd9d51bfb365e4
rnascimento <rodrigo.nascimento@trescon.com.br>
2025-02-19 15:52:25 -0300

    feat: add server and POST endpoint

---
+++ src/server.ts
@@ -0,0 +1,47 @@
+import { fastifyCors } from '@fastify/cors'
+import { fastifySwagger } from '@fastify/swagger'
+import { fastifySwaggerUi } from '@fastify/swagger-ui'
+import { fastify } from 'fastify'
+import {
+  type ZodTypeProvider,
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { env } from './env'
+import { subscribeToEventRoute } from './routes/subscribe-to-event-route'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+// Valida o que foi enviado
+app.setValidatorCompiler(validatorCompiler)
+
+// Edita o que é enviado para o front
+app.setSerializerCompiler(serializerCompiler)
+
+// Cors é aonde a API pode ser utilizado (DEV, HML, PRD e etc)
+app.register(fastifyCors)
+
+// Cria um Swagger para API
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'NLW Connect',
+      version: '0.0.1',
+    },
+  },
+
+  // Faz com que o Swagger formate em JSON as respostas
+  transform: jsonSchemaTransform,
+})
+
+// Cria a UI do Swagger
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
+
+app.register(subscribeToEventRoute)
+
+app.listen({ port: env.PORT }).then(() => {
+  console.log('HTTP server running')
+})
