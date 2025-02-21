8948c780add51b5c56f512c4ccce943b2ddd9db4
rnascimento <rodrigo.nascimento@trescon.com.br>
2025-02-21 18:11:54 -0300

    fix: fix a endpoint description

--- src/routes/get-subscriber-invite-clicks-route.ts
+++ src/routes/get-subscriber-invite-clicks-route.ts
@@ -1,7 +1,5 @@
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 import z from 'zod'
-import { env } from '../env'
-import { accessInviteLink } from '../functions/access-invite-link'
 import { getSubscriberInviteClicks } from '../functions/get-subscriber-invite-clicks'
 
 export const getSubscriberInviteClicksRoute: FastifyPluginAsyncZod =
@@ -12,7 +10,8 @@ export const getSubscriberInviteClicksRoute: FastifyPluginAsyncZod =
         schema: {
           summary: 'Get subscriber invite clicks count',
           tags: ['Referral'],
-          description: 'At this endpoint a invite link is generate for user',
+          description:
+            'At this endpoint you can get a subscriber clicks count ',
           params: z.object({
             subscriberId: z.string(),
           }),
