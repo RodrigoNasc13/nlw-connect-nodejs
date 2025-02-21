998e2a674f15ed7341778f6af33e453aeeda1a8f
rnascimento <rodrigo.nascimento@trescon.com.br>
2025-02-21 14:55:16 -0300

    feat: add referral function

---
+++ src/routes/access-invite-link.ts
@@ -0,0 +1,35 @@
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import z from 'zod'
+import { env } from '../env'
+
+export const accessInviteLinkRoute: FastifyPluginAsyncZod = async app => {
+  app.get(
+    '/invites/:subscriberId',
+    {
+      schema: {
+        summary: 'Access invite link and redirects user',
+        tags: ['Referral'],
+        description: 'At this endpoint a invite link is generate for user',
+        params: z.object({
+          subscriberId: z.string(),
+        }),
+        response: {
+          201: z.object({
+            subscriberId: z.string(),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { subscriberId } = request.params
+
+      console.log(subscriberId)
+
+      const redirectUrl = new URL(env.WEB_URL)
+
+      redirectUrl.searchParams.set('referrer', subscriberId)
+
+      return reply.redirect(redirectUrl.toString(), 302)
+    }
+  )
+}
